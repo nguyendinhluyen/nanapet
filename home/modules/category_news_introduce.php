300a3666dd4a96c41b5da9aa3ba18af43fa5d776
Nguyen Dinh Luyen <luyen.nguyen.itbb@jvn.edu.vn>
2017-02-25 14:16:34 +0700

    update all brand product name

--- home/modules/category_news_introduce.php
+++ home/modules/category_news_introduce.php
@@ -1,9 +1,17 @@
 <?php
+    // Check link thuong hieu san pham
+    $fullUrl = explode("/",$_SERVER['REQUEST_URI']);
+    if (isset($fullUrl[2])) {
+        if ($fullUrl[2] === "tat-ca-thuong-hieu.htm") {
+            $id_brand_name_main = "selected";
+        }
+    }
+    
     $news_key = input($_GET['news_key']);
     if ($news_key == '') {
         $news_key = $News->getFistRowsOfIntroduceByCategory();
     }
-
+    
     $category = $xtemplate->load('category_news_introduce');
     $Cate = new Category();
     $categories = $Cate->getCategoriesParent_introduce();
@@ -17,23 +25,34 @@
                     . "border-width:1px;"
                     . "border-color:#79afc4; width:95%; float:left'>";
         }
-
+        
         // Check the selected category
         $id_danhmuc_main = $categories[$i]['categoriesintroduce_id'];
         if ($news_key == $categories[$i]['categoriesintroduce_key']) {
-            $id_danhmuc_main = "selected";
+            if (empty($id_brand_name_main)) {
+                $id_danhmuc_main = "selected";     
+            }
             $title_page = $categories[$i]['title_category'];
             $description = $categories[$i]['descriptionintroduce'];
             $keywords = $categories[$i]['keywordsintroduce'];
         }
+        
         $tpl.= $xtemplate->assign_vars($block, array(
             'category_name' => $categories[$i]['categoriesintroduce_name'],
             'category_key' => $categories[$i]['categoriesintroduce_key'],
-            'id_danhmuc_main' => $id_danhmuc_main
+            'id_danhmuc_main' => $id_danhmuc_main,
         ));
     }
-
+    $tpl .= "<hr style = 'margin-top: 10px;"
+                    . "margin-bottom: 10px;"
+                    . "border-width:1px;"
+                    . "border-color:#79afc4; width:95%; float:left'>";
     $category = $xtemplate->assign_blocks_content($category, array(
         'CATEGORY' => $tpl
+    ));
+    
+    $category = $xtemplate->replace($category, array(
+        'id_brand_name_main' => $id_brand_name_main,
+        'band_name_product' => "Thương hiệu sản phẩm"
     ));
 ?>
--- home/modules/xtemplate/category_news_introduce.tpl
+++ home/modules/xtemplate/category_news_introduce.tpl
@@ -8,4 +8,9 @@
     </li>
     <!--END_CATEGORY-->
     <!--ENDLIST_CATEGORY-->
+    <li class="danhmuc_main">
+        <a href="{linkS}gioi-thieu/tat-ca-thuong-hieu.htm" 
+           style = "outline:none" 
+           class="{id_brand_name_main}">{band_name_product}</a>
+    </li>
 </ul>
