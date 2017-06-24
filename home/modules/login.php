3eb0f6345817f238aacaa9504c235b4745787f9e
Nguyen Dinh Luyen <luyen.nguyen.itbb@jvn.edu.vn>
2017-06-24 18:54:42 +0700

    just format

--- home/modules/login.php
+++ home/modules/login.php
@@ -1,13 +1,7 @@
-<?php 
-    
+<?php
     $login = $xtemplate->load('login_bootstrap');
-
     $breadcrumbs_path .= '<a href="{linkS}">NanaPet</a>';
-
     $breadcrumbs_path .= ' &raquo; '.'Đăng nhập tài khoản';
-
     $title_page ='Đăng nhập tài khoản';
-
     $content = $login;
 ?>
-
