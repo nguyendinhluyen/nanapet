7a81e862367682fae9a04d3b409358705f9b3575
Nguyen Dinh Luyen <luyen.nguyen.itbb@jvn.edu.vn>
2018-04-07 22:32:40 +0700

    update script assetmanager

--- admincp_login/inv/scripts/assetmanager/settings.php
+++ admincp_login/inv/scripts/assetmanager/settings.php
@@ -1,18 +1,16 @@
-<?
-$bReturnAbsolute = false;
-$sBaseVirtual0 = "/upload"; //Assuming that the path is http://yourserver/Editor/assets/ ("Relative to Root" Path is required)
-$sBase0 = "../../../../upload"; //example for Unix server
-$sName0 = "upload";
-$linkS = '/';
-$sBaseVirtual1 = "";
-$sBase1 = "";
-$sName1 = "";
-
-$sBaseVirtual2 = "";
-$sBase2 = "";
-$sName2 = "";
-
-$sBaseVirtual3 = "";
-$sBase3 = "";
-$sName3 = "";
+<?php
+$bReturnAbsolute = false;
+$sBaseVirtual0 = "/upload";
+$sBase0 = "../../../../upload";
+$sName0 = "upload";
+$linkS = '/';
+$sBaseVirtual1 = "";
+$sBase1 = "";
+$sName1 = "";
+$sBaseVirtual2 = "";
+$sBase2 = "";
+$sName2 = "";
+$sBaseVirtual3 = "";
+$sBase3 = "";
+$sName3 = "";
 ?>
