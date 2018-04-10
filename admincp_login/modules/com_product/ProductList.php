f7dd85f69932607ad6797bf17c5af6ec43589262
Nguyen Dinh Luyen <luyen.nguyen.itbb@jvn.edu.vn>
2018-04-10 22:40:40 +0700

    update copy button admincp product

--- admincp_login/modules/com_product/ProductList.php
+++ admincp_login/modules/com_product/ProductList.php
@@ -1,5 +1,4 @@
 <?php
-
 function formatMoney($number, $fractional = false) {
     if ($fractional) {
         $number = sprintf('%.2f', $number);
@@ -15,13 +14,50 @@ function formatMoney($number, $fractional = false) {
     return $number;
 }
 
+if (($_GET['action'] == 'copy') && isset($_GET['id'])) {
+    $sql = "INSERT INTO products (keywords, description, products_key, products_name
+                        , products_image, view_count, products_price, product_encourage
+                        , price_unit, manufacturer, manufacturer_link, origin
+                        , p_type, p_unit, p_attribute, p_color
+                        , products_status, products_ordered, products_description, description_ads_vi
+                        , product_detail, categories_id, p_new, p_bestsell
+                        , p_encourage, language, username, sort_order
+                        , recycle_bin, species, status, product_detail_tacdung
+                        , product_detail_phuhopcho, product_detail_nguyenlieu_thanhphan
+                        , product_detail_phantichdambao, product_detail_huongdansudung
+                        , product_detail_huongdanbaoquan, product_detail_luuy
+                        , product_detail_khuyenkhich, product_detail_donggoi_thetich
+                        , product_detail_nhasanxuat, product_detail_xuatxu
+                        , upgrade_categories_id, products_date_added)
+                SELECT keywords, description, CONCAT(products_key, '-', MAX(products_id) + 1), CONCAT('(copy of) ',products_name)
+                    , products_image, view_count, products_price, product_encourage
+                    , price_unit, manufacturer, manufacturer_link, origin
+                    , p_type, p_unit, p_attribute, p_color
+                    , products_status, products_ordered, products_description, description_ads_vi
+                    , product_detail, categories_id, p_new, p_bestsell
+                    , p_encourage, language, username, sort_order
+                    , recycle_bin, species, status, product_detail_tacdung
+                    , product_detail_phuhopcho, product_detail_nguyenlieu_thanhphan
+                    , product_detail_phantichdambao, product_detail_huongdansudung
+                    , product_detail_huongdanbaoquan, product_detail_luuy
+                    , product_detail_khuyenkhich, product_detail_donggoi_thetich
+                    , product_detail_nhasanxuat, product_detail_xuatxu
+                    , upgrade_categories_id, products_date_added
+                FROM products
+                WHERE products_id =  " . $_GET['id'];    
+    $mysql->setQuery($sql);
+    $mysql->query();
+    if ($_GET['order'] != "") {
+        header('location:./?show=product&order=' . $_GET['order'] . '&p=' . intval($_GET['p']));
+    } else {
+        header('location:./?show=product&p=' . intval($_GET['p']));
+    }
+}
+
 if (($_GET['action'] == 'del') && isset($_GET['id'])) {
     $id = intval($_GET['id']);
-
     delete_img($id);
-
     delete_product($id);
-
     if ($_GET['order'] != "") {
         header('location:./?show=product&order=' . $_GET['order'] . '&p=' . intval($_GET['p']));
     } else {
@@ -32,32 +68,22 @@ if (($_GET['action'] == 'del') && isset($_GET['id'])) {
 if (isset($_POST['btnfindProduct'])) {
     if (empty($_POST['searchfield']) || $_POST['searchfield'] == "") {
         unset($_SESSION['QUERY_S']);
-
         $_SESSION['QUERY_KEY_S'] = "";
     } else {
         $temp = ($_POST['searchfield'] == '') ? $_GET['keyword'] : $_POST['searchfield'];
-
         unset($_SESSION['QUERY_S']);
-
         $keyword = input($temp);
-
         $where = "AND products_name like '%" . $keyword . "%'";
-
         $_SESSION['QUERY_S'] = $where;
-
         $where = '';
-
         $_SESSION['QUERY_KEY_S'] = $_POST['searchfield'];
     }
 }
 
 if (isset($_POST['btndel'])) {
     $id = (count($_POST['chk']) > 0) ? implode($_POST['chk'], ',') : '';
-
     delete_img($id);
-
     delete_product($id);
-
     if ($_GET['order'] != "") {
         header('location:./?show=product&order=' . $_GET['order'] . '&p=' . intval($_GET['p']));
     } else {
@@ -66,15 +92,10 @@ if (isset($_POST['btndel'])) {
 }
 
 if (isset($_POST['btnencour'])) { // product encourage
-
     $id = (count($_POST['chk']) > 0) ? implode($_POST['chk'], ',') : '';
-
     $sql = "UPDATE products SET p_encourage = 1  WHERE products_id in (" . $id . ")";
-
     $mysql->setQuery($sql);
-
     $mysql->query();
-
     if ($_GET['order'] != "") {
         header('location:./?show=product&order=' . $_GET['order'] . '&p=' . intval($_GET['p']));
     } else {
@@ -84,13 +105,9 @@ if (isset($_POST['btnencour'])) { // product encourage
 
 if (isset($_POST['btnnew'])) {
     $id = (count($_POST['chk']) > 0) ? implode($_POST['chk'], ',') : '';
-
     $sql = "UPDATE products SET p_new = 1 WHERE products_id in (" . $id . ")";
-
     $mysql->setQuery($sql);
-
     $mysql->query();
-
     if ($_GET['order'] != "") {
         header('location:./?show=product&order=' . $_GET['order'] . '&p=' . intval($_GET['p']));
     } else {
@@ -98,15 +115,12 @@ if (isset($_POST['btnnew'])) {
     }
 }
 
-if (isset($_POST['btnbest'])) { //product best seller
+// Product best seller
+if (isset($_POST['btnbest'])) {
     $id = (count($_POST['chk']) > 0) ? implode($_POST['chk'], ',') : '';
-
     $sql = "UPDATE products SET p_bestsell = 1  WHERE products_id in (" . $id . ")";
-
     $mysql->setQuery($sql);
-
     $mysql->query();
-
     if ($_GET['order'] != "") {
         header('location:./?show=product&order=' . $_GET['order'] . '&p=' . intval($_GET['p']));
     } else {
@@ -116,9 +130,7 @@ if (isset($_POST['btnbest'])) { //product best seller
 
 if (isset($_POST['btnstatus'])) {
     $id = (count($_POST['chk']) > 0) ? implode($_POST['chk'], ',') : '';
-
     Set_status_product($id);
-
     if ($_GET['order'] != "") {
         header('location:./?show=product&order=' . $_GET['order'] . '&p=' . intval($_GET['p']));
     } else {
@@ -132,99 +144,68 @@ if (isset($_POST['catlist'])) {
     }
     if ($_POST['catlist'] == 0) {
         unset($_SESSION['QUERY_P']);
-
         unset($_SESSION['CATID']);
     } else {
         $products_list = "";
-
         $catlist = $_POST['catlist'];
-
         $_SESSION['CATID'] = $catlist;
-
         $sql = "SELECT categories_id, level
-					FROM categories
-					WHERE categories_id = " . $catlist;
-
+                FROM categories
+                WHERE categories_id = " . $catlist;
         $mysql->setQuery($sql);
-
         $categories_list = $mysql->loadOneRow();
-
         if ($categories_list['level'] == 1) {
             $sql = "SELECT categories_id
 					    FROM categories
 					    WHERE categories_id = " . $categories_list['categories_id']
                     . " OR parent_id = " . $categories_list['categories_id'];
-
             $mysql->setQuery($sql);
-
             $categories_list_2 = $mysql->loadAllRow();
-
             $categories_list_3 = "";
-
             for ($i = 0; $i < count($categories_list_2); $i++) {
                 if ($i != count($categories_list_2) - 1)
                     $categories_list_3 .= "category_id = " . $categories_list_2[$i]["categories_id"] . " OR ";
                 else
                     $categories_list_3 .= "category_id = " . $categories_list_2[$i]["categories_id"];
             }
-
             $sql = "SELECT product_id
-					    FROM product_category
-					    WHERE " . $categories_list_3 . " GROUP BY product_id ";
+                    FROM product_category
+                    WHERE " . $categories_list_3 . " GROUP BY product_id ";
         }
         else {
             $sql = "SELECT product_id
-					    FROM product_category
-					    WHERE category_id = " . $catlist . " GROUP BY product_id ";
+                    FROM product_category
+                    WHERE category_id = " . $catlist . " GROUP BY product_id ";
         }
-
         $mysql->setQuery($sql);
-
         $products_list = $mysql->loadAllRow();
-
         $list_products_id = " AND ( ";
-
         for ($i = 0; $i < count($products_list); $i++) {
-
             if ($i != (count($products_list) - 1 ))
                 $list_products_id .= ' products_id = ' . $products_list[$i]['product_id'] . " OR ";
             else
                 $list_products_id .= ' products_id = ' . $products_list[$i]['product_id'];
         }
         $list_products_id .= ")";
-
-
         $where = $list_products_id;
-
         $_SESSION['QUERY_P'] = $where;
-
         $where = '';
     }
 }
 
 $title = 'Danh sách sản phẩm';
-
 $xtemplate->path = 'com_product/';
-
 $content = $xtemplate->load('ProductList');
-
 $code_cats = $xtemplate->get_block_from_str($content, "PRODUCT");
-
 if ($_GET['order'] != 'price_asc' && $_GET['order'] != 'price_desc') {
     $p_now = intval($_GET ['p']);
-
-    $sql = "SELECT products_id 
-				FROM products 
-				WHERE language='" . $_SESSION['lag'] . "' " . $_SESSION['QUERY_S'] . $_SESSION['QUERY_P'];
-
+    $sql = "SELECT products_id
+            FROM products
+            WHERE language='" . $_SESSION['lag'] . "' " . $_SESSION['QUERY_S'] . $_SESSION['QUERY_P'];
     $mysql->setQuery($sql);
-
     $mysql->query();
-
     $total = $mysql->getNumRows();
-
     $numofpages = $total / $pp;
-
     if ($p_now <= 0) {
         $page = 1;
     } else {
@@ -235,102 +216,62 @@ if ($_GET['order'] != 'price_asc' && $_GET['order'] != 'price_desc') {
     }
 
     $limitvalue = $page * $pp - ($pp);
-
     switch ($_GET['order']) {
         case 'name':
-
             $orderby = ' products_name asc';
-
             break;
-
         case 'name_desc':
-
             $orderby = ' products_name desc';
-
             break;
-
         case 'status':
-
             $orderby = ' products_status asc';
-
             break;
-
         case 'dateadd':
-
             $orderby = ' products_date_added asc';
-
             break;
         case 'dateadd_desc':
-
             $orderby = ' products_date_added desc';
-
             break;
-
         case 'datemodifiled':
-
             $orderby = ' modified_date desc';
-
             break;
-
         default:
-
             $orderby = ' products_id desc';
-
             break;
     }
 
-    $sql = "SELECT * 
-            FROM products 
-            WHERE language='" . $_SESSION['lag'] . "' " . $_SESSION['QUERY_S'] . $_SESSION['QUERY_P'] 
+    $sql = "SELECT *
+            FROM products
+            WHERE language='" . $_SESSION['lag'] . "' " . $_SESSION['QUERY_S'] . $_SESSION['QUERY_P']
             . " ORDER BY $orderby limit $limitvalue,$pp";
-
     $mysql->setQuery($sql);
-
     $row = $mysql->loadAllRow();
-
     $n = count($row);
-
     $temp = '';
-
     $begin = 0;
-
     $end = $n;
-
-    //Lay gia khuyen mai lam gia chinh cho san pham
+    // Get encourage price
     for ($i = 0; $i < $n; ++$i) {
         if ($row[$i]['product_encourage'] != "") {
             $row[$i]['products_price'] = $row[$i]['product_encourage'];
         }
     }
-}
-
-//VI TRI THEM DIEU CHINH SAP XEP TANG DAN HOAC GIAM DAN THEO GIA
-else {
-
+} else {
     function orderByASC($data, $field) {
         $code = "return strnatcmp(\$a['$field'], \$b['$field']);";
         usort($data, create_function('$a,$b', $code));
         return $data;
     }
-
-    //Phan trang
-
+    // Pagination
     $p_now = intval($_GET ['p']);
-
-    $sql = "SELECT * 
-				FROM products 
-				WHERE language='" . $_SESSION['lag'] . "' " . $_SESSION['QUERY_S'] . $_SESSION['QUERY_P'];
-
+    $sql = "SELECT *
+            FROM products
+            WHERE language='" . $_SESSION['lag'] . "' " . $_SESSION['QUERY_S'] . $_SESSION['QUERY_P'];
     $mysql->setQuery($sql);
-
     $mysql->query();
-
     $total = $mysql->getNumRows();
-
     $row = $mysql->loadAllRow();
-
     $numofpages = $total / $pp;
-
     if ($p_now <= 0) {
         $page = 1;
     } else {
@@ -339,39 +280,28 @@ else {
         else
             $page = 1;
     }
-
-    //end phan trang																														
-
+    // End pagination
     $temp = '';
-
     $begin = ($page - 1) * $pp;
-
     $end = $page * $pp;
-
     if ($page == ceil($numofpages)) {
         $end = count($row);
     }
-
     $n = count($row);
-
     for ($i = 0; $i < $n; ++$i) {
         if ($row[$i]['product_encourage'] != "") {
             $row[$i]['products_price'] = $row[$i]['product_encourage'];
         }
-
         $row[$i]['products_price'] = str_replace(".", '', $row[$i]['products_price']);
-
         $row[$i]['products_price'] = floatval($row[$i]['products_price']);
     }
     if ($_GET['order'] != 'price_desc')
         $row = orderByASC($row, 'products_price');
-
     else {
         $row = orderByASC($row, 'products_price');
         $row = array_reverse($row);
     }
-
-    // Sau khi sap xep xong format lai tien
+    // Format money
     for ($i = $begin; $i < $end; ++$i) {
         $row[$i]['products_price'] = formatMoney($row[$i]['products_price']);
     }
@@ -379,17 +309,14 @@ else {
 
 for ($i = $begin; $i < $end; ++$i) {
     $color = ($i % 2 == 0) ? '#d5d5d5' : '#e5e5e5';
-
     $img = $row[$i]['products_image'];
-
     $imgSrc = _UPLOAD_IMG . $img;
-
     $imgThumb = _UPLOAD_IMG_THUMB . $img;
-
     if (!empty($img) and ! checkExtentFile($img, $imgExtent)) {
         $img = '<a href="' . _UPLOAD_IMG_THUMB . $img . '">File</a>';
     } else {
-        $img = ($img == '') ? "<img src ='" . _STYLE_IMG . "picoff.gif'>" : "<a href ='$imgThumb'><img border=0 style=\"cursor:hand\" src=\"" . _STYLE_IMG . "picon.gif\" onmouseover=\"this.src='" . _STYLE_IMG . "piconover.gif';return overlib('<div><table border=0 cellpadding=2 cellspacing=0><tr><td class=titleImg>Click to Thumbnails</td></tr><tr><td><img src=$imgSrc></a></td></tr></table></div>');\" onmouseout=\"this.src='" . _STYLE_IMG . "picon.gif';return nd();\"></a>";
+        $img = ($img == '') ? "<img src ='" . _STYLE_IMG . "picoff.gif'>" : "<a href ='$imgThumb'><img border=0 style=\"cursor:hand\" src=\""
+        . _STYLE_IMG . "picon.gif\" onmouseover=\"this.src='" . _STYLE_IMG . "piconover.gif';return overlib('<div><table border=0 cellpadding=2 cellspacing=0><tr><td class=titleImg>Click to Thumbnails</td></tr><tr><td><img src=$imgSrc></a></td></tr></table></div>');\" onmouseout=\"this.src='" . _STYLE_IMG . "picon.gif';return nd();\"></a>";
     }
 
     $temp.= $xtemplate->assign_vars($code_cats, array(
@@ -398,7 +325,8 @@ for ($i = $begin; $i < $end; ++$i) {
         'categories_id' => CatlistUpgrade_2($row[$i]['upgrade_categories_id']),
         'product_image' => $img,
         'sort_order' => $row[$i]['sort_order'],
-        'products_status' => ($row[$i]['products_status'] == '1') ? $arr_lang['show'] : '<i>' . $arr_lang['hidden'] . '</i>',
+        'products_status' => ($row[$i]['products_status'] == '1') ? $arr_lang['show'] : '<i>' 
+                        . $arr_lang['hidden'] . '</i>',
         'date_added' => date('G:i d/m/Y', $row[$i]['products_date_added']),
         'modified_date' => ($row[$i]['modified_date'] == '0') ? '0' : date('G:i d/m/Y', $row[$i]['modified_date']),
         'price_product' => $row[$i]['products_price'] . ' VNĐ',
@@ -407,7 +335,6 @@ for ($i = $begin; $i < $end; ++$i) {
 }
 
 $content = $xtemplate->assign_blocks_content($content, array("PRODUCT" => $temp));
-
 $content = $xtemplate->replace($content, array(
     'lag_product_manager'   => $title,
     'suggestionText'        => $suggestionText,
@@ -416,7 +343,8 @@ $content = $xtemplate->replace($content, array(
     'lag_page'              => $arr_lang['page'],
     'p'                     => ($_GET['p'] == '') ? '1' : intval($_GET['p']),
     'oderby'                => $_GET['order'],
-    'page'                  => page_div("./?show=product&p=%d_pg&order=" . $_GET['order'], "10", ceil($numofpages), $page),
+    'page'                  => page_div("./?show=product&p=%d_pg&order=" 
+                            . $_GET['order'], "10", ceil($numofpages), $page),
     'lag_delete'            => $arr_lang['del'],
     'lag_edit'              => $arr_lang['edit'],
     'catlist'               => cat_fillter($_SESSION['CATID']),
--- admincp_login/modules/com_product/ProductList.tpl
+++ admincp_login/modules/com_product/ProductList.tpl
@@ -78,7 +78,7 @@
 
     h3 {
         margin: 0px;
-        padding: 0px;	
+        padding: 0px;
     }
 
     .suggestionsBox {
@@ -89,7 +89,7 @@
         background-color: #212427;
         -moz-border-radius: 7px;
         -webkit-border-radius: 7px;
-        border: 2px solid #000;	
+        border: 2px solid #000;
         color: #fff;
         z-index: 100;
     }
@@ -117,18 +117,17 @@
     <div>{lag_page} : {page}</div>
     <div style="float:left;width:450px;margin:5px;padding-top:20px">
         <b style="margin-right: 10px">{defaultText}</b>
-        <input value = "{productKey}" 
+        <input value = "{productKey}"
                type="text"
-               size="20" 
-               name = "searchfield" 
+               size="20"
+               name = "searchfield"
                id="inputString"
                onkeyup="lookup(this.value);"
-               onblur="fill();"/>
-
-        <input type ='submit' 
-               name ='btnfindProduct' 
-               value ='Search' 
-               class ='button'/>
+               onblur="fill();">
+        <input type ='submit'
+               name ='btnfindProduct'
+               value ='Search'
+               class ='button'>
     </div>
     <div class="suggestionsBox" id="suggestions" style="display: none;">
         <img src="../style/images/upArrow.png" style="position: relative; top: -12px; left: 30px;" alt="upArrow" />
@@ -137,80 +136,74 @@
         </div>
     </div>
     <div style="margin:5px;padding-top:20px;float:left">Bộ lọc : {catlist}</div>
-    <div style="clear:both"></div>    
-    <div style = "float:left">			                                    
+    <div style="clear:both"></div>
+    <div style = "float:left">
         <input class = "button" type = "submit" name = "btnstatus" value = "{shi}" onclick = "return chkselected();"/>
         <input class = "button" style="display: none;" type = "submit" name = "btnnew" value = "Sản phẩm mới" onclick = "return chkselected();"/>
         <input class = "button" style="display: none;" type = "submit" name = "btnencour" value = "Sản phẩm khuyến mãi" onclick = "return chkselected();"/>
         <input class = "button" type = "submit" name = "btnbest" value = "Sản phẩm bán chạy" onclick = "return chkselected();"/>
-        <input class = "button" type = "button" value = "Thêm mới" onclick="window.location = './?show=OptProduct&order={oderby}'"/>        
-        <div class = "title" style = 'width:851px; margin-top: 5px; margin-bottom: 5px'>				
-            <div class = "con_title" style = "width:50px;text-align:center">                
-                <input style ='margin-left:3px' type="checkbox" name="chkall" onclick="chkallClick(this);"></div>				
-            <div class = "con_title" style = "width:50px">&nbsp;</div>				
-            <div class = "con_title" style = "width:50px">{lag_edit}</div>				
-            <div class = "con_title" style = "width:180px;">
+        <input class = "button" type = "button" value = "Thêm mới" onclick="window.location = './?show=OptProduct&order={oderby}'"/>
+        <div class = "title" style = 'width:851px; margin-top: 5px; margin-bottom: 5px'>
+            <div class = "con_title" style = "width:50px;text-align:center">
+                <input style ='margin-left:3px' type="checkbox" name="chkall" onclick="chkallClick(this);"></div>
+            <div class = "con_title" style = "width:50px">{lag_edit}</div>
+            <div class = "con_title" style = "width:220px;">
                 <a href = './?show=product&order=name&p={p}' style="text-decoration:underline">{productname} (a->z)</a>
                 <span>&nbsp&nbsp-&nbsp </span>
                 <span>
                     <a href = './?show=product&order=name_desc&p={p}'style = "text-decoration:underline">(z->a)</a>
                 </span>
             </div>
-
             <div class = "con_title" style = "width:70px">{image}</div>
-
             <div class = "con_title" style = "width:160px;">Thuộc danh mục</div>
-
             <div class = "con_title" style = "width:60px;">
                 <a href = './?show=product&order=status&p={p}'style='text-decoration:underline'>{lag_status}</a>
             </div>
-
             <div class = "con_title" style = "width:110px;border:0px;">
                 <a href = './?show=product&order=dateadd&p={p}' style="text-decoration:underline">Ngày (cũ)</a>
-                <span>&nbsp&nbsp-&nbsp </span>
+                <span>&nbsp&nbsp-&nbsp</span>
                 <span>
                     <a href = './?show=product&order=dateadd_desc&p={p}'style = "text-decoration:underline">(mới)</a>
                 </span>
             </div>
-
-            <!-- Sort theo gia san pham-->                
+            <!-- Sort theo gia san pham-->
             <div class = "con_title" style = "width:auto;text-decoration:underline">
-                <a href = './?show=product&order=price_desc&p={p}'>Giá Giảm </a>
+                <a href = './?show=product&order=price_desc&p={p}'>Giảm </a>
             </div>
-
             <div class = "con_title" style = "width:auto;text-decoration:none">&nbsp-&nbsp </div>
+            <div class = "con_title" style = "width:auto;text-decoration:underline"><a href = './?show=product&order=price_asc&p={p}'> Tăng</a></div>
+            <div class = "con_title" style = "width:60px; text-align:center">Copy</div>
 
-            <div class = "con_title" style = "width:auto;text-decoration:underline"><a href = './?show=product&order=price_asc&p={p}'> Giá Tăng</a></div>                 
-
-        </div>                                                            
+        </div>
         <div>
             <!--BEGINLIST_PRODUCT-->
             <!--BEGIN_PRODUCT-->
             <div class = "con_list_con" style = "width:50px;background-color:{color};height:50px;text-align:center">
-                <input style ='margin-top:8px' type="checkbox" name="chk[]" value="{id}"></div>
-                <!--<div class = "con_list_con" style = "width:50px;background-color:{color};height:50px;">
-                    <a href="?show=product&action=del&id={id}&p={p}&order={oderby}" onclick="return confirm('{del_confirm} ?');">{lag_delete}</a>
-                </div>-->
-            <div class = "con_list_con" style = "width:50px;background-color:{color};height:50px;">
-                &nbsp;
+                <input style ='margin-top:8px' type="checkbox" name="chk[]" value="{id}">
             </div>
+            <!--<div class = "con_list_con" style = "width:50px;background-color:{color};height:50px;">
+                <a href="?show=product&action=del&id={id}&p={p}&order={oderby}" onclick="return confirm('{del_confirm} ?');">{lag_delete}</a>
+            </div>-->
             <div class = "con_list_con" style = "width:50px;background-color:{color};height:50px;">
                 <a href="?show=OptProduct&id={id}&p={p}&order={oderby}">{lag_edit}</a>
             </div>
-            <div class = "con_list_con" style = "width:180px;background-color:{color};height:50px;">{product_name}</div>
+            <div class = "con_list_con" style = "width:220px;background-color:{color};height:50px;">{product_name}</div>
             <div class = "con_list_con" style = "width:50px;background-color:{color};height:50px;">{product_image}</div>
             <div class = "con_list_con" style = "width:180px;background-color:{color};height:50px;">{categories_id}</div>
             <div class = "con_list_con" style = "width:60px;background-color:{color};height:50px">{products_status}</div>
             <div class = "con_list_con" style = "width:110px;border:0px;background-color:{color};height:50px">{date_added}</div>
-            <div class = "con_list_con" style = "width:120px;border:0px;background-color:{color};height:50px">
+            <div class = "con_list_con" style = "width:80px;border:0px;background-color:{color};height:50px">
                 {price_product}
+            </div>
+            <div class = "con_list_con" style = "width:50px;background-color:{color};height:50px;">
+                <a href="?show=product&action=copy&id={id}&p={p}&order={oderby}">Copy</a>
             </div>
             <div class ='clear'></div>
             <!--END_PRODUCT-->
             <!--ENDLIST_PRODUCT-->
         </div>
     </div>
-<!--<input class = "button" type = "submit" name = "btndel" value = "{delete_check}" onclick="return chkdelete('{del_confirm}');"/>-->    
+<!--<input class = "button" type = "submit" name = "btndel" value = "{delete_check}" onclick="return chkdelete('{del_confirm}');"/>-->
     <input class = "button" type = "submit" name = "btnstatus" value = "{shi}" onclick = "return chkselected();"/>
     <input class = "button" style="display: none;" type = "submit" name = "btnnew" value = "Sản phẩm mới" onclick = "return chkselected();"/>
     <input class = "button" style="display: none;" type = "submit" name = "btnencour" value = "Sản phẩm khuyến mãi" onclick = "return chkselected();"/>
